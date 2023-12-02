a0d5d2a2f71763aabe2f135810daf9a6bbbe84d5
usefSaeed <youssef.okab@gmail.com>
2023-12-02 17:42:16 +0200

    create DFA files

---
+++ DFA/DFA.cpp
@@ -0,0 +1,5 @@
+//
+// Created by usef on 12/2/2023.
+//
+
+#include "DFA.h"
---
+++ DFA/DFA.h
@@ -0,0 +1,23 @@
+//
+// Created by usef on 12/2/2023.
+//
+
+#ifndef COMPILER_DFA_H
+#define COMPILER_DFA_H
+
+#include <string>
+#include <utility>
+#include <iostream>
+#include "algorithm"
+
+class DFA{
+
+    private:
+
+
+    public:
+
+
+};
+
+#endif //COMPILER_DFA_H
